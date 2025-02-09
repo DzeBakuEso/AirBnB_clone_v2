dcb02833e551ed9c07533503504afa3a557a350f
DzeBakuEso <dzeblebaku@gmail.com>
2025-02-09 15:23:53 -0800

    models/engine/file_storage.py, models/engine/db_storage.py, models/state.py

--- web_flask/models/engine/db_storage.py
+++ web_flask/models/engine/db_storage.py
@@ -1,9 +1,62 @@
-from sqlalchemy.orm import sessionmaker, scoped_session
+#!/usr/bin/python3
+"""Database storage engine"""
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from models.base_model import Base
+from models.state import State
+from models.city import City
+import os
 
 class DBStorage:
-    """Interacts with the MySQL database"""
+    """Handles long term storage of data in a MySQL database"""
+
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Initialize the database connection"""
+        user = os.getenv('HBNB_MYSQL_USER')
+        pwd = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST')
+        db = os.getenv('HBNB_MYSQL_DB')
+        self.__engine = create_engine(f'mysql+mysqldb://{user}:{pwd}@{host}/{db}',
+                                      pool_pre_ping=True)
+
+        if os.getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """Query objects from the database"""
+        if cls:
+            return {f"{obj.__class__.__name__}.{obj.id}": obj
+                    for obj in self.__session.query(cls).all()}
+        else:
+            objects = {}
+            for model in [State, City]:  # Add other models if necessary
+                for obj in self.__session.query(model).all():
+                    objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+            return objects
+
+    def new(self, obj):
+        """Add a new object to the session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commit all changes to the database"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Delete an object from the session"""
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Reload the database session"""
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(session_factory)
 
     def close(self):
-        """Call remove() method on the private session attribute"""
+        """Close the current session"""
         self.__session.remove()
--- web_flask/models/state.py
+++ web_flask/models/state.py
@@ -1,18 +1,22 @@
+#!/usr/bin/python3
+"""State Model"""
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 from models.city import City
-import models
+from models import storage
+from os import getenv
 
+class State(BaseModel, Base):
+    """Representation of a State"""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
 
-class State:
-    """State class"""
-
-    @property
-    def cities(self):
-        """
-        Getter method that returns the list of City objects linked to the current State.
-        """
-        if models.storage_type != 'db':
-            return [
-                city for city in models.storage.all(City).values()
-                if city.state_id == self.id
-            ]
-        return []
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="all, delete")
+    else:
+        @property
+        def cities(self):
+            """Return list of City instances linked to the current State"""
+            from models import storage
+            return [city for city in storage.all(City).values() if city.state_id == self.id]
