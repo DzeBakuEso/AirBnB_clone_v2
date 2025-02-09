31340abd4600cf5128e286166b9b50ad6b354f8a
DzeBakuEso <dzeblebaku@gmail.com>
2025-02-09 15:10:36 -0800

    models/engine/file_storage.py, models/engine/db_storage.py, models/state.p

--- web_flask/models/engine/db_storage.py
+++ web_flask/models/engine/db_storage.py
@@ -1,7 +1,9 @@
 from sqlalchemy.orm import sessionmaker, scoped_session
 
+
 class DBStorage:
     """Interacts with the MySQL database"""
+
     def close(self):
         """Call remove() method on the private session attribute"""
         self.__session.remove()
--- web_flask/models/state.py
+++ web_flask/models/state.py
@@ -1,11 +1,18 @@
 from models.city import City
 import models
 
+
 class State:
     """State class"""
+
     @property
     def cities(self):
-        """Getter method that returns the list of City objects linked to the current State"""
+        """
+        Getter method that returns the list of City objects linked to the current State.
+        """
         if models.storage_type != 'db':
-            return [city for city in models.storage.all(City).values() if city.state_id == self.id]
+            return [
+                city for city in models.storage.all(City).values()
+                if city.state_id == self.id
+            ]
         return []
